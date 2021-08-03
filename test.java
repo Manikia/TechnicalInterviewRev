98eacb7559cd390c6a799a653a5eb0d92f2c2977
Monika <monikaangel.ma15@gmail.com>
2021-08-02 20:38:34 -0700

    practice

---
+++ test.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        for (int i = 0; i < prices.length; i++)
+        // running through the array
+        {
+            while (prices != null)
+            // as long as its not the end of the array do this loop
+            {
+                if (prices[i] < (prices[i] + 1))
+            // if the current is less than the next one then i wanna save it
+                {
+                    
+                }
+            }
+        }
+        return prices;
+    }
+}
