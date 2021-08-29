6692adb1cd04bf84eb84849babae44205c0694c2
Monika <monikaangel.ma15@gmail.com>
2021-08-28 21:23:30 -0700

    continued py lesson

--- learning py again, sad/lesson1.py
+++ learning py again, sad/lesson1.py
@@ -27,12 +27,48 @@ print(name[-1]) #maybe we can
 # if we substitute the 1 w an i and increase
 print(name[-12])
 
-newstr = "this-is a string"
+newstr = "this-is-a-string"
 print(newstr[0:4]) #slices to only have 'this'
 print(newstr[8:len(newstr)]) 
 # prints everything from beginning to the end by
 # getting the string length 
 #[start:end:step]
+print("Skipping splits")
 print(newstr[0:5])
 print(newstr[0:5:2])
 print(newstr[0:5:4])
+
+#reversing a string
+#if we reverse a string we switch the start and end indices
+print(newstr[len(newstr):0:-1])
+
+#to print the whole string we do
+print(newstr[:])
+
+#to reverse the whole string we do
+print(newstr[::-1])
+
+#we can do floor division which gets you the smallest float integer
+print(5.5//4.5)
+
+#is checks if two things are the same objects
+#and is not check if theyre not
+x = ["apple", "banana", "cherry"]
+y = x
+print(x is y)
+
+#=	Assign
+#+= Add
+#-= Subtract
+#*= Multiply
+#/= Divide
+#//= Divide, Floor
+#**= Raise power
+#%= Take Modulo
+#|= OR
+#&= AND
+#^= XOR
+#>>= Right-shift
+#<<= Left-shift
+
+
